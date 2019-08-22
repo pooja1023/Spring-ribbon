839132a254bd56710baa5f0e8a9e951527036856
Pooja <poojachiluvuri@gmail.com>
2019-08-22 15:13:23 +0530

    commit

--- src/main/java/com/verizon/springribbonclient/SpringRibbonClientApplication.java
+++ src/main/java/com/verizon/springribbonclient/SpringRibbonClientApplication.java
@@ -5,15 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Import;
 
 import com.netflix.client.config.IClientConfig;
 import com.verizon.springribbonclient.config.RibbonConfiguration;
+import com.verizon.springribbonclient.queue.ActiveQueueListener;
+import com.verizon.springribbonclient.queue.FanoutListener;
+import com.verizon.springribbonclient.queue.TopicListener;
 import com.verizon.springribbonclient.rest.RestEndpoint;
+import com.verizon.springribbonclient.spring.RibbonClientConfiguration;
 
-@SpringBootApplication(scanBasePackageClasses = {com.netflix.client.config.IClientConfig.class,RestEndpoint.class})
+@SpringBootApplication(scanBasePackageClasses = {IClientConfig.class,RestEndpoint.class,ActiveQueueListener.class,TopicListener.class,FanoutListener.class})
 @RibbonClient(name="ping-a-server",configuration=RibbonConfiguration.class)
 @EnableEurekaClient
 @EnableCircuitBreaker
+@Import(value=RibbonClientConfiguration.class)
 public class SpringRibbonClientApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/verizon/springribbonclient/rest/RestEndpoint.java
+++ src/main/java/com/verizon/springribbonclient/rest/RestEndpoint.java
@@ -1,5 +1,7 @@
 package com.verizon.springribbonclient.rest;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -14,6 +16,8 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 public class RestEndpoint {
 	@Autowired
 	private LoadBalancerClient loadbalclient;
+	@Autowired
+	private RabbitTemplate rabbittemp;
 @RequestMapping
 @HystrixCommand(fallbackMethod = "fallbackMessageEndpoint")
 public String consumeMessage() {
@@ -23,6 +27,16 @@ public String consumeMessage() {
 	int port=serviceinst.getPort();
 	String response=resttemplate.getForObject("http://"+host+":"+port+"/message", String.class);
 	return response;
+}
+@RequestMapping("/readMsg")
+public String readMessage() {
+	Message message = rabbittemp.receive("directqueue");
+	return new String(message.getBody());
+}
+@RequestMapping("/readMsg2")
+public String readMessage2() {
+	Message message = rabbittemp.receive("directqueue2");
+	return new String(message.getBody());
 }
 public String fallbackMessageEndpoint()
 {
